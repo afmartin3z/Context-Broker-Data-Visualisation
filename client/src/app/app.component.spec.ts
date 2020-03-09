1b628b1b711748d8239366c6120e3d1c25d751ed
Victor Espinosa Mateu <vicesmat@gmail.com>
2020-03-09 11:59:48 +0100

    trying jest

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,42 +1,35 @@
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import {
-    async,
-    fakeAsync,
-    tick,
-    ComponentFixture,
-} from '@angular/core/testing';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-
-import { ConfigureFn, configureTests } from "../app/shared/config.helpers";
-
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-    let fixture: ComponentFixture<AppComponent>;
-    let component: AppComponent;
-
-    beforeEach((() => {
-        const configure: ConfigureFn = testBed => {
-            testBed.configureTestingModule({
-                declarations: [AppComponent],
-                imports: [NoopAnimationsModule, HttpClientTestingModule],
-                // schemas: [NO_ERRORS_SCHEMA],
-            });
-        };
-
-        configureTests(configure).then(testBed => {
-            fixture = testBed.createComponent(AppComponent);
-            component = fixture.componentInstance;
-            fixture.detectChanges();
-        });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule
+            ],
+            declarations: [
+                AppComponent
+            ],
+        }).compileComponents();
     }));
 
-    it('should create the app', async(() => {
-        const app = component;
+    it('should create the app', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
         expect(app).toBeTruthy();
-    }));
+    });
 
+    it(`should have as title 'CB-Baseline'`, () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('CB-Baseline');
+    });
+
+    it('should render title', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('.content span').textContent).toContain('CB-Baseline app is running!');
+    });
 });
-
-test.todo('a sample todo');
